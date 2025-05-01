77c1e8a2242ea84ce1c05d570dfdff9c37008ad9
arpitbhardwaj10 <104090555+arpitbhardwaj10@users.noreply.github.com>
2025-05-02 01:37:05 +0530

    Add files via upload

---
+++ AreaofTriangle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int b,h;
+    
+    cout << "base value" << endl;
+    cin >> b;
+    cout << "height value" << endl; 
+    cin >> h;
+
+        int area = (b*h)/2;
+        cout << "The Area of the Traingle is " << area << endl;
+      
+
+        return 0;
+
+    
+}
