bc369a221d4ad7ee696f0c8ab90473517703a623
arpitbhardwaj10 <104090555+arpitbhardwaj10@users.noreply.github.com>
2025-05-15 08:21:29 +0530

    Add files via upload

---
+++ AreaofCircle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float r, area;
+    cout << " Enter radius of a circle" << endl;
+    cin >> r;
+    
+    area = 3.14f * r * r;
+    cout << "Area of Circle " << area << endl;
+    
+    return 0;
+
+}
